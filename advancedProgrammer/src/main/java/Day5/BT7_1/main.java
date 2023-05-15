21a08a20494b0ac458b9b2ceaafcf6d3a85517e9
hoangthanh <phucthanh290803@gmail.com>
2023-05-15 19:52:30 +0700

    Bài tập ôn tập

---
+++ advancedProgrammer/src/main/java/Day5/BT7_1/main.java
@@ -0,0 +1,16 @@
+package Day5.BT7_1;
+
+public class main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("John Doe", true, "Premium");
+        double serviceExpense = 100.0;
+        double productExpense = 50.0;
+
+        Visit visit = new Visit(customer, serviceExpense, productExpense);
+        System.out.println(visit);
+
+        double totalExpense = visit.getTotalExpense();
+        System.out.println("Total Expense: $" + totalExpense);
+    }
+
+}
